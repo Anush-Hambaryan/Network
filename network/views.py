cf59d13990c2aea59dafad3df5c1d1862cc4ade2
Anush-Hambaryan <anushhambaryan@gmail.com>
2021-03-24 20:34:09 +0400

    Reformat views.py

--- network/views.py
+++ network/views.py
@@ -5,31 +5,25 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.core.paginator import Paginator
-
 from .models import User, Post, Comment, Profile
 
 
 def index(request):
     return render(request, "network/index.html")
 
-
 def new_post(request):
-
     if request.method == "POST":
         data = json.loads(request.body)
         text = data.get("post")
         post = Post(user=request.user, post=text)
         post.save()
         return JsonResponse({"message": "Posted successfully."}, status=201)
-
     return JsonResponse({"error": "POST request required."}, status=400)
-
 
 def post(request, id):
     if request.method == "GET":
         post = Post.objects.get(pk=id)
-        comments = Comment.objects.filter(
-            post=post).order_by("timestamp").all()
+        comments = Comment.objects.filter(post=post).order_by("timestamp").all()
         return JsonResponse([comment.serialize() for comment in comments], safe=False)
     elif request.method == "PUT":
         data = json.loads(request.body)
@@ -50,8 +44,7 @@ def post(request, id):
         data = json.loads(request.body)
         if data.get("comment") is not None:
             post = Post.objects.get(pk=id)
-            comment = Comment(user=request.user, post=post,
-                              comment=data.get("comment"))
+            comment = Comment(user=request.user, post=post, comment=data.get("comment"))
             comment.save()
             return JsonResponse(comment.serialize(), safe=False)
     elif request.method == "DELETE":
@@ -63,15 +56,13 @@ def post(request, id):
             Comment.objects.get(pk=data.get("comment_id")).delete()
             return JsonResponse({"message": "Comment deleted successfully."}, status=201)
 
-
 def list_posts(request, key):
     try:
         if key == "all":
             posts = Post.objects.order_by("-timestamp").all()
             return JsonResponse([post.serialize() for post in posts], safe=False)
         elif key[-10:] == "-following":
-            posts = Post.objects.filter(user__in=request.user.profile.get(
-                user=request.user).following.all()).order_by("-timestamp").all()
+            posts = Post.objects.filter(user__in=request.user.profile.get(user=request.user).following.all()).order_by("-timestamp").all()
             return JsonResponse([post.serialize() for post in posts], safe=False)
         else:
             user = User.objects.get(username=key)
@@ -79,7 +70,6 @@ def list_posts(request, key):
             return JsonResponse([post.serialize() for post in posts], safe=False)
     except Post.DoesNotExist:
         return JsonResponse({"error": "Posts not found."}, status=404)
-
 
 def profile(request, username):
     if request.method == "GET":
@@ -92,8 +82,7 @@ def profile(request, username):
         return JsonResponse(profile.serialize(), safe=False)
     elif request.method == "PUT":
         user = User.objects.get(username=username)
-        follower = User.objects.get(
-            username=json.loads(request.body).get("follower"))
+        follower = User.objects.get(username=json.loads(request.body).get("follower"))
         user_profile = Profile.objects.get(user=user)
         follower_profile = Profile.objects.get(user=follower)
         if json.loads(request.body).get("value") == "follow":
@@ -103,7 +92,6 @@ def profile(request, username):
             user_profile.follower.remove(follower)
             follower_profile.following.remove(user)
         return JsonResponse(user_profile.serialize(), safe=False)
-
 
 def login_view(request):
     if request.method == "POST":
@@ -122,24 +110,20 @@ def login_view(request):
     else:
         return render(request, "network/login.html")
 
-
 def logout_view(request):
     logout(request)
     return render(request, "network/index.html")
-
 
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
-
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
             return render(request, "network/register.html", {
                 "message": "Passwords must match."
             })
-
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
